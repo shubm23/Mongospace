4dd460addfc647c7c724679405e93916795fbe18
shubm23 <shubm23@gmail.com>
2018-10-19 16:19:03 +0530

    Second Commit

--- test/test_helper.js
+++ test/test_helper.js
@@ -4,7 +4,10 @@ const mongoose = require('mongoose');
 before((done)=>{
   mongoose.connect('mongodb://localhost/users_test', { useNewUrlParser: true });
   mongoose.connection
-    .once('open', () => { done(); })
+    .once('open', () => {
+      console.log('connected to mongodb');
+      done();
+    })
     .on('error', (error) => {
       console.warn('// WARNING: ',error);
     });
